30e925ae1b10b8d8ac778f0de0b0d2528d3a6050
Jan Lindström <janm.lindstrom@live.com>
2024-07-03 17:07:20 +0300

    Add switch service

--- src/types.d.ts
+++ src/types.d.ts
@@ -4,6 +4,8 @@ type FanActiveState = 0 | 1;
 
 type FanRotationSpeedState = 0 | 1 | 2 | 3 | 4 | 5;
 
+type SwitchOnState = 0 | 1;
+
 type RegisterAddress = `${3 | 4}x${number}${number}${number}${number}${number}`;
 
 type RegisterType = 3 | 4;
