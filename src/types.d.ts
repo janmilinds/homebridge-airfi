827e86a6c689ba3286367f7081423dc5c8420195
Jan Lindström <janm.lindstrom@live.com>
2024-07-04 21:57:47 +0300

    Update types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,15 +1,14 @@
-import 'webrtc';
+export type FanActiveState = 0 | 1;
 
-type FanActiveState = 0 | 1;
+export type FanRotationSpeedState = 0 | 1 | 2 | 3 | 4 | 5;
 
-type FanRotationSpeedState = 0 | 1 | 2 | 3 | 4 | 5;
+export type SwitchOnState = 0 | 1;
 
-type SwitchOnState = 0 | 1;
+export type RegisterAddress =
+  `${3 | 4}x${number}${number}${number}${number}${number}`;
 
-type RegisterAddress = `${3 | 4}x${number}${number}${number}${number}${number}`;
+export type RegisterType = 3 | 4;
 
-type RegisterType = 3 | 4;
-
-type WriteQueue = {
+export type WriteQueue = {
   [key: RegisterAddress]: number;
 };
