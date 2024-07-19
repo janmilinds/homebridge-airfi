06281597100d3e73ee898d5be491c104740df650
Jan Lindström <janm.lindstrom@live.com>
2024-07-19 20:08:58 +0300

    Make AirfiService class a default export

--- src/services/AirfiInformationService.ts
+++ src/services/AirfiInformationService.ts
@@ -1,6 +1,6 @@
 import { CharacteristicValue, PlatformAccessory } from 'homebridge';
 
-import { AirfiService } from './AirfiService';
+import AirfiService from './AirfiService';
 import { AirfiHomebridgePlatform } from '../AirfiHomebridgePlatform';
 import { RegisterAddress, ServiceOptions } from '../types';
 
--- src/services/index.ts
+++ src/services/index.ts
@@ -4,14 +4,12 @@ import AirfiInformationService from './AirfiInformationService';
 import AirfiSwitchService from './AirfiSwitchService';
 import AirfiTemperatureSensorService from './AirfiTemperatureSensorService';
 import AirfiThermostatService from './AirfiThermostatService';
-import { AirfiService } from './AirfiService';
 
 export {
   AirfiFanService,
   AirfiHumiditySensorService,
   AirfiInformationService,
   AirfiSwitchService,
-  AirfiService,
   AirfiTemperatureSensorService,
   AirfiThermostatService,
 };
